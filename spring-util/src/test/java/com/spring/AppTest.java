07966452a3ae7b0da5cf3ff86159ed4382351450
wangzhe01 <wangzhe01@Koolearn-inc.com>
2019-12-07 21:16:24 +0800

    update

--- spring-util/src/test/java/com/spring/AppTest.java
+++ spring-util/src/test/java/com/spring/AppTest.java
@@ -1,10 +1,12 @@
 package com.spring;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +14,9 @@ import org.junit.Test;
 import com.spring.excel.entity.StudentEntity;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.ImportParams;
 
 /**
  * Unit test for simple App.
@@ -34,5 +38,15 @@ public class AppTest {
         FileOutputStream fos = new FileOutputStream("C:/Users/wangzhe01/Desktop/1.xls");
         workbook.write(fos);
         fos.close();
+    }
+
+    @Test
+    public void test2() {
+        ImportParams params = new ImportParams();
+        params.setTitleRows(0);
+        params.setHeadRows(1);
+        File file = new File("C:\\Users\\wangzhe01\\Desktop", "1.xls");
+        List<StudentEntity> list = ExcelImportUtil.importExcel(file, StudentEntity.class, params);
+        System.out.println(list);
     }
 }
